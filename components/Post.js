8c2ead8ea38087b9e54c44ef427a5a6584239e45
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-01-08 00:55:14 -0500

    [add] single post content

---
+++ components/Post.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/prefetch';
+
+
+function Post(props) {
+  return (
+    <article className="panel panel-info">
+      <header className="panel-heading">
+        <h2 className="panel-title">{props.title}</h2>
+      </header>
+      <div
+        className="panel-body"
+        dangerouslySetInnerHTML={{
+          __html: props.full ? props.content : props.excerpt,
+        }}
+      />
+      {props.full || (
+        <div className="panel-footer">
+          <Link href={`/post?id=${props.ID}`}>
+            Leer más
+          </Link>
+        </div>
+      )}
+    </article>
+  );
+}
+
+
+export default Post;
